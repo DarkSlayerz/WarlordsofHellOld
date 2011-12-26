dfb9145b4115a0af207ea99421b752d54fbe3b12
Bas van Driel <darkslayerzz@hotmail.com>
2011-12-26 16:53:50 +0100

    git@github.com:DarkSlayerz/WarlordsofHellOld.git

--- Client - Cache/Class/Class/Play.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-Title Client
-cd ./Class/
-START java -Xmx1000m client 30 0 lowmem members 32
-exit
--- Client - Cache/Class/Compiler.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-Title Compiler
-"C:\Program Files\Java\jdk1.6.0_25\bin\javac.exe" -d ./Class/ ./Source/*.java ./Source/sign/*.java
-pause
